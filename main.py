54d7ec5e3adb4438f7018eddbd05ec4c2f1e9b0f
Kamil_Radzimowski <kamil.radzimowski@onet.eu>
2021-11-11 11:48:11 +0100

    Comments added

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter
-
-# Basic variables
 from tkinter import RIGHT, BOTH, X, TOP, BOTTOM, CENTER, LEFT
 
+# Basic variables
 buttons_grid = []
 rows = 10
 columns = 10
@@ -40,7 +39,7 @@ def move_window(event):
 def maximize_window():
     global is_window_maximized
     if is_window_maximized:
-        window.geometry('700x850')
+        window.geometry('700x650')
         window.state('normal')
         is_window_maximized = False
     else:
@@ -48,6 +47,7 @@ def maximize_window():
         is_window_maximized = True
 
 
+# initialize items on a title bar
 title_bar = tkinter.Frame(window, bg=title_bar_color, relief="raised")
 close_button = tkinter.Button(title_bar, text="X", command=window.destroy, bg=title_bar_color, fg="white",
                               borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
@@ -55,21 +55,28 @@ maximize_button = tkinter.Button(title_bar, text="[  ]", command=maximize_window
                                  borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
 title_label = tkinter.Label(title_bar, bg=title_bar_color, text="PathFinding GUI", fg="white", borderwidth=0,
                             pady=3, padx=10)
+python_logo = tkinter.PhotoImage(file="python_small_logo.png")
+logo_label = tkinter.Label(title_bar, bg=title_bar_color, pady=3, borderwidth=0, padx=10, image=python_logo)
 
+
+# place items on a title bar
 title_bar.pack(side=TOP, expand=False, fill=X)
 close_button.pack(side=RIGHT)
 maximize_button.pack(side=RIGHT)
 title_label.pack(side=LEFT)
+logo_label.pack(side=LEFT)
 title_bar.bind('<B1-Motion>', move_window)
 title_label.bind('<B1-Motion>', move_window)
 title_bar.bind('<Button-1>', mouse_click)
 
+# Define top frame (play button etc)
 frame_top = tkinter.Frame(window)
 frame_top.pack(side=TOP, fill=X)
 frame_top.config(bg=background_color)
 
+# Define bottom frame (main grid)
 frame_bottom = tkinter.Frame(window)
-frame_bottom.pack(side=BOTTOM, fill=X, padx=100, pady=50)
+frame_bottom.pack(side=BOTTOM, fill=None, expand=1, pady=50)
 frame_bottom.config(bg=background_color)
 
 play_button = tkinter.Button(frame_top, bg="white", text="P", height=button_height, width=button_width).pack()

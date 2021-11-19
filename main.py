01037fcf0df70faeb7f7096678905439d7e3ec80
DzikiOwoc15 <kamil.radzimowski@onet.eu>
2021-11-19 22:41:53 +0100

    added assets folder

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter
-from tkinter import RIGHT, BOTH, X, TOP, BOTTOM, CENTER, LEFT
+from tkinter import RIGHT, BOTH, X, TOP, BOTTOM, CENTER, LEFT, Y
 
 # Basic variables
 buttons_grid = []
+state_matrix = []
 rows = 10
 columns = 10
 button_height = 3
 button_width = 5
 is_window_maximized = False
+default_geometry='800x900'
 
 # Define colors used
 title_bar_color = "#121212"
@@ -22,7 +24,13 @@ window.configure(bg=background_color)
 
 # Make a custom title bar
 window.overrideredirect(True)
-window.geometry('700x650')
+window.geometry(default_geometry)
+
+
+def calc_path(grid):
+    foo = "foo"
+    # do sth
+    # do more
 
 
 def mouse_click(event):
@@ -39,47 +47,12 @@ def move_window(event):
 def maximize_window():
     global is_window_maximized
     if is_window_maximized:
-        window.geometry('700x650')
+        window.geometry(default_geometry)
         window.state('normal')
         is_window_maximized = False
     else:
         window.state('zoomed')
         is_window_maximized = True
-
-
-# initialize items on a title bar
-title_bar = tkinter.Frame(window, bg=title_bar_color, relief="raised")
-close_button = tkinter.Button(title_bar, text="X", command=window.destroy, bg=title_bar_color, fg="white",
-                              borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
-maximize_button = tkinter.Button(title_bar, text="[  ]", command=maximize_window, bg=title_bar_color, fg="white",
-                                 borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
-title_label = tkinter.Label(title_bar, bg=title_bar_color, text="PathFinding GUI", fg="white", borderwidth=0,
-                            pady=3, padx=10)
-python_logo = tkinter.PhotoImage(file="python_small_logo.png")
-logo_label = tkinter.Label(title_bar, bg=title_bar_color, pady=3, borderwidth=0, padx=10, image=python_logo)
-
-
-# place items on a title bar
-title_bar.pack(side=TOP, expand=False, fill=X)
-close_button.pack(side=RIGHT)
-maximize_button.pack(side=RIGHT)
-title_label.pack(side=LEFT)
-logo_label.pack(side=LEFT)
-title_bar.bind('<B1-Motion>', move_window)
-title_label.bind('<B1-Motion>', move_window)
-title_bar.bind('<Button-1>', mouse_click)
-
-# Define top frame (play button etc)
-frame_top = tkinter.Frame(window)
-frame_top.pack(side=TOP, fill=X)
-frame_top.config(bg=background_color)
-
-# Define bottom frame (main grid)
-frame_bottom = tkinter.Frame(window)
-frame_bottom.pack(side=BOTTOM, fill=None, expand=1, pady=50)
-frame_bottom.config(bg=background_color)
-
-play_button = tkinter.Button(frame_top, bg="white", text="P", height=button_height, width=button_width).pack()
 
 
 # Function to change color when button is clicked
@@ -91,6 +64,44 @@ def button_click(x_axis, y_axis):
     else:
         current_button.config(background=button_color)
 
+
+# initialize items on a title bar
+title_bar = tkinter.Frame(window, bg=title_bar_color, relief="raised")
+close_button = tkinter.Button(title_bar, text="X", command=window.destroy, bg=title_bar_color, fg="white",
+                              borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
+maximize_button = tkinter.Button(title_bar, text="[  ]", command=maximize_window, bg=title_bar_color, fg="white",
+                                 borderwidth=0, activebackground=button_clicked_color, pady=3, padx=3)
+title_label = tkinter.Label(title_bar, bg=title_bar_color, text="PathFinding GUI", fg="white", borderwidth=0,
+                            pady=3, padx=10)
+python_logo = tkinter.PhotoImage(file="assets/python_small_logo.png")
+logo_label = tkinter.Label(title_bar, bg=title_bar_color, borderwidth=0, image=python_logo)
+
+# place items on a title bar
+logo_label.pack(side=LEFT, padx=5)
+title_bar.pack(side=TOP, expand=False, fill=X)
+close_button.pack(side=RIGHT)
+maximize_button.pack(side=RIGHT)
+title_label.pack(side=LEFT)
+title_bar.bind('<B1-Motion>', move_window)
+title_label.bind('<B1-Motion>', move_window)
+title_bar.bind('<Button-1>', mouse_click)
+logo_label.bind('<B1-Motion>', move_window)
+
+
+# Define top frame (play button etc)
+frame_top = tkinter.Frame(window)
+frame_top.pack(side=TOP, pady=25)
+frame_top.config(bg=background_color)
+
+# Define bottom frame (main grid)
+frame_bottom = tkinter.Frame(window)
+frame_bottom.pack(side=BOTTOM, fill=None, expand=1, pady=50)
+frame_bottom.config(bg=background_color)
+
+play_button_image = tkinter.PhotoImage(file="assets/baseline_play_circle_white_24dp.png")
+play_button = tkinter.Button(frame_top, image=play_button_image, bg=background_color, highlightthickness=0, bd=0,
+                             activebackground=background_color,
+                             command=lambda grid=state_matrix: calc_path(grid)).pack()
 
 # Draw the board (Including the row and column labels)
 for x in range(rows + 1):

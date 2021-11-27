b001580ec54700962829a2e66618b2bdfe1cda83
Kamil_Radzimowski <kamil.radzimowski@onet.eu>
2021-11-27 13:46:47 +0100

    Path finding alogrithm WIP

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ import pyautogui
 
 # buttons grid is a list containing all buttons for a future reference
 buttons_grid = []
-# state matrix is a list containing states of all buttons (clicked or not)
+# state matrix is a list containing states of all matrix buttons (clicked or not)
 # False meaning not pressed
 state_matrix = []
+move_list = []
 rows = 10
 columns = 10
 button_height = 2
 button_width = 5
 start_x = None
 start_y = None
+finish_x = None
+finish_y = None
 is_window_maximized = False
 is_set_start_button_pressed = False
 is_set_finish_button_pressed = False
 default_geometry = '900x1000'
-window_previous_y = 0
-window_previous_x = 0
 
 # Define colors used
 title_bar_color = "#121212"
@@ -39,11 +40,24 @@ window.overrideredirect(True)
 window.geometry(default_geometry)
 
 
-def calc_path(grid):
-    # TODO CREATE ALGORITHM THAT FINDS THE SHORTEST PATH
-    foo = "foo"
-    # do sth
-    # do more
+def calc_path():
+    global finish_x, finish_y, start_y, start_x, move_list
+    move_list = []
+    move_list.append((finish_x, finish_y, 0))
+    for move in move_list:
+        check_adjacent_cells(move)
+
+
+def check_adjacent_cells(move):
+    current_x = move[0]
+    current_y = move[1]
+    counter = move[2]
+    global move_list
+    # Check up
+    if current_x - 1 >= 0:
+        if not matrix_list[current_x - 1][current_y]:
+            #do sth
+
 
 
 def move_window(event):
@@ -71,34 +85,40 @@ def maximize_window():
         is_window_maximized = True
 
 
-def set_start_button_click(self):
+def set_start_button_click(self, button_to_unclick):
     global is_set_start_button_pressed, is_set_finish_button_pressed
     if is_set_start_button_pressed:
-        print("button unclicked")
         is_set_start_button_pressed = False
         self.config(relief=RAISED)
     else:
-        print("button clicked")
+        # Unclick the finish button
+        is_set_finish_button_pressed = False
+        button_to_unclick.config(relief=RAISED)
+        # Click the start button
         is_set_start_button_pressed = True
         self.config(relief=SUNKEN)
 
 
-def set_finish_button(self):
+def set_finish_button_click(self, button_to_unclick):
     global is_set_finish_button_pressed, is_set_start_button_pressed
     if is_set_finish_button_pressed:
         is_set_finish_button_pressed = False
         self.config(relief=RAISED)
     else:
-        # TODO ONLY ONE BUTTON CAN BE SUNKEN AT THE SAME TIME
+        # Unclick the start button
+        button_to_unclick.config(relief=RAISED)
+        is_set_start_button_pressed = False
+        # Click the finish button
         is_set_finish_button_pressed = True
         self.config(relief=SUNKEN)
 
 
 # Function to change color and state when button is clicked
-def button_click(x_axis, y_axis):
-    global is_set_start_button_pressed, start_x, start_y
+def matrix_button_click(x_axis, y_axis):
+    # TODO FIX BUG - CLICKING ON A FINISH/START BUTTON CHANGES COLOR BUT DOES NOT REMOVE POSITION VARIABLES
+    global is_set_start_button_pressed, start_x, start_y, finish_x, finish_y
     current_button = buttons_grid[x_axis - 1][y_axis - 1]
-    if not is_set_start_button_pressed:
+    if not is_set_start_button_pressed and not is_set_finish_button_pressed:
         button_clicked = state_matrix[x_axis - 1][y_axis - 1]
         if not button_clicked:
             state_matrix[x_axis - 1][y_axis - 1] = True
@@ -106,11 +126,22 @@ def button_click(x_axis, y_axis):
         else:
             state_matrix[x_axis - 1][y_axis - 1] = False
             current_button.config(background=button_color)
-    else:
-        # TODO COMPLETE SET START
+    elif is_set_start_button_pressed:
+        # Unclick previous start button
+        if start_x is not None:
+            old_start_button = buttons_grid[start_x][start_y]
+            old_start_button.config(background=button_color)
         start_x = x_axis - 1
         start_y = y_axis - 1
         current_button.config(background="green")
+    elif is_set_finish_button_pressed:
+        # Unclick previous finish button
+        if finish_x is not None:
+            old_finish_button = buttons_grid[finish_x][finish_y]
+            old_finish_button.config(background=button_color)
+        finish_x = x_axis - 1
+        finish_y = y_axis - 1
+        current_button.config(background="red")
 
 
 # initialize items on a title bar
@@ -148,20 +179,22 @@ frame_bottom.config(bg=background_color)
 play_button_image = tkinter.PhotoImage(file="assets/baseline_play_circle_white_24dp.png")
 play_button = tkinter.Button(frame_top, image=play_button_image, bg=background_color, highlightthickness=0, bd=0,
                              activebackground=background_color,
-                             command=lambda grid=state_matrix: calc_path(grid)).pack()
+                             command=lambda : calc_path()).pack()
+
 start_button = tkinter.Button(frame_top)
 start_button.pack()
-start_button.config(bg="green", text="Set start",
-                    command=lambda self=start_button:
-                    set_start_button_click(self),
-                    activebackground="green")
-start_button.bind('<Button-1>', set_start_button_click(start_button))
+# start_button.bind('<Button-1>', set_start_button_click(start_button))
 finish_button = tkinter.Button(frame_top)
 finish_button.pack()
+
 finish_button.config(bg="red", text="Set Finish",
                      activebackground="red",
-                     command=lambda self=finish_button:
-                     set_finish_button(self))
+                     command=lambda self=finish_button, button_to_unclick=start_button:
+                     set_finish_button_click(self, start_button))
+start_button.config(bg="green", text="Set start",
+                    command=lambda self=start_button, button_to_unclick=finish_button:
+                    set_start_button_click(self, button_to_unclick),
+                    activebackground="green")
 
 # Draw the board (Including the row and column labels)
 for x in range(rows + 1):
@@ -182,7 +215,7 @@ for x in range(rows + 1):
             matrix_list.append(False)
             button = tkinter.Button(frame_bottom, bg=button_color, activebackground=button_clicked_color,
                                     height=button_height, width=button_width,
-                                    command=lambda z=x, w=y: button_click(z, w))
+                                    command=lambda z=x, w=y: matrix_button_click(z, w))
             button.grid(row=x, column=y)
             button_list.append(button)
     if len(button_list) != 0:
